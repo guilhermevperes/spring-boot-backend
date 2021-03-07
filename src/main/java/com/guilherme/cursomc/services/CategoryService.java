3786a22fba7b9f886cf82ed1d324e2ad15eb8c8a
Guilherme Peres <guilherme.vperes@gmail.com>
2021-03-06 22:34:38 -0300

    Paginacao com parametro opcionais

--- src/main/java/com/guilherme/cursomc/services/CategoryService.java
+++ src/main/java/com/guilherme/cursomc/services/CategoryService.java
@@ -10,6 +10,9 @@ import com.guilherme.cursomc.services.exceptions.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -46,6 +49,11 @@ public class CategoryService {
 
     public List<Category> findAll() {
         return categoryRepository.findAll();
+    }
+
+    public Page<Category> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+        return categoryRepository.findAll(pageRequest);
     }
 
 }
