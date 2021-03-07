4a06d3416fc1d2aece62cf19a9f69cb6e2d3f992
Guilherme Peres <guilherme.vperes@gmail.com>
2021-03-07 18:25:41 -0300

    Validando se email ja existe na criacao de cliente

--- src/main/java/com/guilherme/cursomc/repositories/ClientRepository.java
+++ src/main/java/com/guilherme/cursomc/repositories/ClientRepository.java
@@ -4,8 +4,12 @@ import com.guilherme.cursomc.domain.Client;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+    @Transactional(readOnly = true)
+    Client findByEmail(String email);
 
 }
--- src/main/java/com/guilherme/cursomc/services/validation/InsertClientValidator.java
+++ src/main/java/com/guilherme/cursomc/services/validation/InsertClientValidator.java
@@ -2,16 +2,24 @@ package com.guilherme.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import com.guilherme.cursomc.DTO.ClientNewDTO;
 import com.guilherme.cursomc.domain.enums.ClientType;
+import com.guilherme.cursomc.repositories.ClientRepository;
 import com.guilherme.cursomc.resources.exceptions.FieldMessage;
 import com.guilherme.cursomc.services.validation.utils.BR;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class InsertClientValidator implements ConstraintValidator<InsertClient, ClientNewDTO> {
+
+    @Autowired
+    ClientRepository clientRepository;
+
     @Override
     public void initialize(InsertClient ann) {
 
@@ -28,6 +36,11 @@ public class InsertClientValidator implements ConstraintValidator<InsertClient, 
 
         if (objDTO.getType().equals(ClientType.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDTO.getCpfCnpj())) {
             FieldMessage fieldMessage = new FieldMessage("cpfCnpj", "CNPJ inválido.");
+            list.add(fieldMessage);
+        }
+
+        if (!Objects.isNull(clientRepository.findByEmail(objDTO.getEmail()))) {
+            FieldMessage fieldMessage = new FieldMessage("email", "Email já cadastrado.");
             list.add(fieldMessage);
         }
 
